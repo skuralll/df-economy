685e0655d10d8f93207d13304674cf14dcf5899b
skuralll <fuji.ktbl@gmail.com>
2025-06-12 12:08:20 +0000

    service: add GetBalance method

--- economy/service/service.go
+++ economy/service/service.go
@@ -1,8 +1,10 @@
 package service
 
 import (
+	"context"
 	"database/sql"
 
+	"github.com/google/uuid"
 	"github.com/skuralll/dfeconomy/internal/db"
 )
 
@@ -14,4 +16,20 @@ type EconomyService struct {
 func NewEconomyService(dbsql *sql.DB) *EconomyService {
 	dbInstance := db.NewSQLite(dbsql) // TODO: Support multiple databases
 	return &EconomyService{dbInstance}
+}
+
+// Set balance
+// Set(ctx context.Context, id uuid.UUID, name *string, amount float64) error
+// Get balance ranking
+// Top(ctx context.Context, page, size int) ([]EconomyEntry, error)
+
+// TODO: Move validation logic in db to the service. The db should only operate the database based on the received values.
+
+// Get balance
+func (svc *EconomyService) GetBalance(ctx context.Context, id uuid.UUID) (float64, error) {
+	amount, err := svc.db.Balance(ctx, id)
+	if err != nil {
+		return 0, err
+	}
+	return amount, nil
 }
