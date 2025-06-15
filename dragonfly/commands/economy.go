cb2b86e12450dc76ccb6034eed1047b263678c5f
skuralll <fuji.ktbl@gmail.com>
2025-06-15 12:40:42 +0000

    feat: implement balance command

--- dragonfly/commands/economy.go
+++ dragonfly/commands/economy.go
@@ -1,9 +1,12 @@
 package commands
 
 import (
+	"context"
+
 	"github.com/df-mc/dragonfly/server/cmd"
 	"github.com/df-mc/dragonfly/server/player"
 	"github.com/df-mc/dragonfly/server/world"
+	"github.com/google/uuid"
 )
 
 // /economy
@@ -29,6 +32,27 @@ func (e EconomyBalanceCommand) Run(src cmd.Source, o *cmd.Output, tx *world.Tx) 
 	if !ok {
 		o.Error("Execute as a player")
 	}
+	// get target uuid
+	tn := e.Username.LoadOr(p.Name())
+	var uid uuid.UUID
+	if tn == p.Name() {
+		uid = p.UUID()
+	} else {
+		// get uuid by name
+		var err error
+		uid, err = e.svc.GetUUIDByName(context.Background(), tn)
+		if err != nil {
+			o.Error("Player not found: " + tn)
+			return
+		}
+	}
+	// get balance
+	amount, err := e.svc.GetBalance(context.Background(), uid)
+	if err != nil {
+		o.Error("Failed to get balance")
+		return
+	}
+	o.Printf("Balance of %s: %.2f", tn, amount)
 
 	o.Printf("Executed: /economy balance %s", e.Username.LoadOr(p.Name()))
 }
