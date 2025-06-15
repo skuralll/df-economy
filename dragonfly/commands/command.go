e475b49359dd005d8a3f44ee0ddbcc13e80ab892
skuralll <fuji.ktbl@gmail.com>
2025-06-15 08:10:26 +0000

    commands: add EconomyBalanceCommand skeleton

--- dragonfly/commands/command.go
+++ dragonfly/commands/command.go
@@ -3,5 +3,5 @@ package commands
 import "github.com/df-mc/dragonfly/server/cmd"
 
 func RegisterCommands() {
-	cmd.Register(cmd.New("economy", "Displays economy-related information.", nil, EconomyCommand{}))
+	cmd.Register(cmd.New("economy", "Displays economy-related information.", nil, &EconomyCommand{}, &EconomyBalanceCommand{}))
 }
--- dragonfly/commands/economy.go
+++ dragonfly/commands/economy.go
@@ -2,15 +2,34 @@ package commands
 
 import (
 	"github.com/df-mc/dragonfly/server/cmd"
+	"github.com/df-mc/dragonfly/server/player"
 	"github.com/df-mc/dragonfly/server/world"
 )
 
+// /economy
+
 type EconomyCommand struct{}
 
-// Run implements cmd.Runnable.
 func (c EconomyCommand) Run(src cmd.Source, o *cmd.Output, tx *world.Tx) {
-	o.Printf("実行されました: /economy")
+	o.Printf("Executed: /economy")
 }
 
-// Validate implements cmd.Runnable.
+// /economy balance <target>
+
+type EconomyBalanceCommand struct {
+	SubCmd   cmd.SubCommand       `cmd:"balance" help:"Displays the balance of a player."`
+	Username cmd.Optional[string] `cmd:"username"`
+}
+
+func (e EconomyBalanceCommand) Run(src cmd.Source, o *cmd.Output, tx *world.Tx) {
+	p, ok := src.(*player.Player)
+	if !ok {
+		o.Error("Execute as a player")
+	}
+
+	o.Printf("Executed: /economy balance %s", e.Username.LoadOr(p.Name()))
+}
+
+// Validation
 var _ cmd.Runnable = (*EconomyCommand)(nil)
+var _ cmd.Runnable = (*EconomyBalanceCommand)(nil)
