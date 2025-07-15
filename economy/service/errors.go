5b4ec57f61986515ffd111c6a9af380c6b9b5343
skuralll <fuji.ktbl@gmail.com>
2025-07-15 11:34:11 +0000

    Add error : ErrValidation

--- economy/service/errors.go
+++ economy/service/errors.go
@@ -7,8 +7,13 @@ import (
 
 var (
 	ErrPlayerExists = errors.New("player already exists")
+	ErrValidation   = errors.New("validation error")
 )
 
 func NewPlayerExistsError(id string) error {
 	return fmt.Errorf("%w: player ID %s already exists", ErrPlayerExists, id)
+}
+
+func NewValidationError(field, message string) error {
+	return fmt.Errorf("%w: %s %s", ErrValidation, field, message)
 }
